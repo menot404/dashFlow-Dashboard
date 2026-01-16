736630afe7e3ca11e1366802e6d186b154c6c692
Me_Not404 <m3not404@gmail.com>
2026-01-16 09:42:14 +0000

    add: file context/index.js

---
+++ src/context/index.js
@@ -0,0 +1,9 @@
+// Export des Providers
+export { AuthProvider } from './auth/AuthProvider'
+export { ThemeProvider } from './theme/ThemeProvider'
+export { NotificationProvider } from './notification/NotificationProvider'
+
+// Export des Contextes (utile pour les tests)
+export { AuthContext } from './auth/AuthContext'
+export { ThemeContext } from './theme/ThemeContext'
+export { NotificationContext } from './notification/NotificationContext'
