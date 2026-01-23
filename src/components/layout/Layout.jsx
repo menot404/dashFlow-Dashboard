0e9021ba3dd8982f010f5ce13d82b9cd318ed7c3
Me_Not404 <m3not404@gmail.com>
2026-01-23 21:35:08 +0000

    feat: add Breadcrumbs common

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom'
 import Sidebar from './Sidebar';
 import Header from './Header';
 import { Menu } from 'lucide-react'
+import Breadcrumbs from '../common/Breadcrumbs';
 const Layout = () => {
     const [sidebarOpen, setSidebarOpen] = useState(false)
 
@@ -29,6 +30,7 @@ const Layout = () => {
             <div className="flex-1 flex flex-col w-full min-w-0">
                 <Header onMenuClick={() => setSidebarOpen(true)} />
                 <main className="flex-1 p-4 sm:p-6 overflow-auto">
+                    <Breadcrumbs/>
                     <Outlet />
                 </main>
             </div>
